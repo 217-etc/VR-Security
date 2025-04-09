75c26525e1f713d3d7d93cc507d1ac36e3398be9
Yoon KyoungMin <yoongang02@gmail.com>
2025-04-09 16:30:01 +0900

    Feat : 완강기 통 구성품 손 촬영 & Fix: Tool 이름 UI 다시 나타나는 오류 해결

--- Assets/03.Scripts/UI/Tool Info/ToolUIManager.cs
+++ Assets/03.Scripts/UI/Tool Info/ToolUIManager.cs
@@ -14,7 +14,13 @@ public class ToolUIManager : MonoBehaviour
     }
     public void DisappearUI()
     {
+        if (toolGrabManager.IsGrabEnd(toolName)) return;
         animator.SetTrigger("Disappear");
         toolGrabManager.GrabTool(toolName);
+
+
+        // Rigidbody 키네틱 해제
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().useGravity = true;
     }
 }
